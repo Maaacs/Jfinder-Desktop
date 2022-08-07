ed0ee94e5961d531686eefbc8b3f644e19a41588
Maaacs <msb@icomp.ufam.edu.br>
2022-08-06 20:41:04 -0400

    Add Css

---
+++ src/main/java/com/example/jfinder/UsuarioController.java
@@ -0,0 +1,17 @@
+package com.example.jfinder;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class UsuarioController implements Initializable{
+
+
+
+    public void initialize(URL url, ResourceBundle resource){
+
+    }
+}
+
